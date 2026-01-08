f29497923fbb8824237c5ec67c254933bdb4ece1
github-action-benchmark <github@users.noreply.github.com>
2026-01-08 12:59:02 +0000

    add Circuit Size (customSmallerIsBetter) benchmark result for 85fb44a8d313ea8fbeef46f64df777597fbbc5d0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767877141260,
+  "lastUpdate": 1767877142284,
   "repoUrl": "https://github.com/noir-lang/eth-proofs",
   "entries": {
     "ACIR Opcodes": [
@@ -49,6 +49,57 @@ window.BENCHMARK_DATA = {
             "name": "benchmark_get_transaction.json/main",
             "value": 282856,
             "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ],
+    "Circuit Size": [
+      {
+        "commit": {
+          "author": {
+            "email": "26174818+jialinli98@users.noreply.github.com",
+            "name": "Jialin Li",
+            "username": "jialinli98"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85fb44a8d313ea8fbeef46f64df777597fbbc5d0",
+          "message": "feat: add benchmarks + update library version (#13)",
+          "timestamp": "2026-01-08T12:56:36Z",
+          "tree_id": "d171552acae7156ec0175eef5ec3b28d84010893",
+          "url": "https://github.com/noir-lang/eth-proofs/commit/85fb44a8d313ea8fbeef46f64df777597fbbc5d0"
+        },
+        "date": 1767877142199,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark_get_account.json/main",
+            "value": 1490405,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "benchmark_get_header.json/main",
+            "value": 200015,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "benchmark_get_log.json/main",
+            "value": 1706806,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "benchmark_get_receipt.json/main",
+            "value": 1196846,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "benchmark_get_transaction.json/main",
+            "value": 1276784,
+            "unit": "circuit_size"
           }
         ]
       }
