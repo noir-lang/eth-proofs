7c291a3f1bbf44d2ef0f7c23cc5f2138376f3232
github-action-benchmark <github@users.noreply.github.com>
2026-01-08 12:59:01 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 85fb44a8d313ea8fbeef46f64df777597fbbc5d0

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767877141260,
+  "repoUrl": "https://github.com/noir-lang/eth-proofs",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "email": "26174818+jialinli98@users.noreply.github.com",
+            "name": "Jialin Li",
+            "username": "jialinli98"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85fb44a8d313ea8fbeef46f64df777597fbbc5d0",
+          "message": "feat: add benchmarks + update library version (#13)",
+          "timestamp": "2026-01-08T12:56:36Z",
+          "tree_id": "d171552acae7156ec0175eef5ec3b28d84010893",
+          "url": "https://github.com/noir-lang/eth-proofs/commit/85fb44a8d313ea8fbeef46f64df777597fbbc5d0"
+        },
+        "date": 1767877140915,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark_get_account.json/main",
+            "value": 217667,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "benchmark_get_header.json/main",
+            "value": 35438,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "benchmark_get_log.json/main",
+            "value": 355880,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "benchmark_get_receipt.json/main",
+            "value": 217834,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "benchmark_get_transaction.json/main",
+            "value": 282856,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
